cf80a73f5b763ca771a50b7c689da561661fb32a
Akollek <amiel.kollek@mail.mcgill.ca>
2016-10-27 18:52:53 -0400

    cnn applied to 60x60 images

--- main.py
+++ main.py
@@ -4,7 +4,47 @@ Team ATP
 """
 from __future__ import division
 
+import csv
+
+from skimage import filters
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from models.basic_cnn.cnn import train
+
+
+def view_image(image):
+    plt.imshow(image, cmap='Greys_r')
+    plt.show()
+
+
+def load_dataset():
+    X = np.fromfile('data/train_X.bin', dtype='uint8')
+    X = X.reshape(-1,1,60,60)
+
+    # really simply background removal (for now)
+    X = np.apply_along_axis(lambda im: (im > 250).astype(np.float32), 0, X)
+
+    with open('data/train_y.csv','r') as f:
+        r = csv.reader(f)
+        next(r,None)
+        Y = np.array([int(val) for idx, val in r], dtype='uint8')
+
+    # put aside 10k for validation
+    X_train, Y_train = X[:-10000], Y[:-10000]
+    X_val, Y_val = X[-10000:], Y[-10000:]
+
+    return X_train, Y_train, X_val, Y_val
+    
+
+
 
 if __name__ == "__main__":
-    pass
+    X_train, y_train, X_val, y_val = load_dataset()
+    
+    train(X_train, y_train, X_val, y_val, 2)
+
+
 
---
+++ models/basic_cnn/cnn.py
@@ -0,0 +1,179 @@
+"""
+cnn.py
+
+This file contains the code which trains a 
+Convolutional NN on the full 60x60 pixel
+images of our data set.
+
+Note: This file is heavily inspired by:
+ https://raw.githubusercontent.com/Lasagne/Lasagne/master/examples/mnist.py
+
+"""
+from __future__ import division
+
+import sys
+import os
+import time
+
+import numpy as np
+import theano
+import theano.tensor as T
+
+import lasagne
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+model_file = os.path.join(dir_path, 'basic_cnn_model.npz')
+
+def errprint(*args):
+    sys.stderr.write('\n'.join(map(str,args)) + '\n')
+
+def build_cnn(input_var):
+    network = lasagne.layers.InputLayer(
+            shape=(None, 1, 60, 60),
+            input_var=input_var)
+
+    # convolutional layer
+    # using the settings given in the lasagne example
+    network = lasagne.layers.Conv2DLayer(
+            network,
+            num_filters=32,
+            filter_size=(10,10),
+            nonlinearity=lasagne.nonlinearities.rectify,
+            W=lasagne.init.GlorotUniform())
+
+    # now add a dense layer with dropout 
+    #   NOTE: Maybe we don't want dropout (or less dropout), since the 
+    #         digits in our data are the same so we might want to overfit
+    #         a little bit...
+    network = lasagne.layers.DenseLayer(
+            lasagne.layers.dropout(network, p=0.5),
+            num_units=19,
+            nonlinearity=lasagne.nonlinearities.softmax)
+
+    return network
+
+
+# iterator for shuffled batches
+def batches(inputs, targets, batchsize):
+    assert len(inputs) == len(targets)
+    indices = np.arange(len(inputs))
+    np.random.shuffle(indices)
+    for idx in range(0, len(inputs) - batchsize + 1, batchsize):
+        batch = indices[idx:idx + batchsize]
+        yield inputs[batch], targets[batch]
+
+def train(X_train, y_train, X_val, y_val, epochs):
+    if os.path.exists(model_file):
+        errprint("Model file exists. Please delete it and run again")
+        return
+
+    input_var = T.tensor4('inputs')
+    target_var = T.ivector('targets')
+
+    errprint('Building model and functions')
+
+    network = build_cnn(input_var)
+    prediction = lasagne.layers.get_output(network)
+    
+    # Cross entropy loss function for multiclass network
+    loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
+    loss = loss.mean()
+
+    # Updates for SGD
+    params = lasagne.layers.get_all_params(network, trainable=True)
+    updates = lasagne.updates.nesterov_momentum(
+            loss,
+            params,
+            learning_rate=0.01,
+            momentum=0.9)
+
+
+    valid_prediction = lasagne.layers.get_output(network, deterministic=True)
+    valid_loss = lasagne.objectives.categorical_crossentropy(
+            valid_prediction,
+            target_var).mean()
+
+    valid_acc = T.mean(T.eq(T.argmax(valid_prediction, axis=1), target_var),
+            dtype=theano.config.floatX)
+
+    train_fn = theano.function([input_var, target_var], loss, updates=updates)
+    val_fn = theano.function([input_var, target_var], [valid_loss, valid_acc])
+
+
+    errprint('Starting training')
+
+    # write intermediate param values to temporary file
+    tmp_model_file = model_file[:-4] + '.tmp.npz'
+    errprint("Writing intermediate params to: %s" % tmp_model_file)
+
+    for epoch in xrange(epochs):
+        train_err = 0
+        train_batches = 0
+        start_time = time.time()
+        train_pass = [ 
+                train_fn(xs, ys) for xs, ys in batches(X_train, y_train, 500)
+        ]
+        train_err = sum(train_pass)
+        train_batches = len(train_pass)
+
+        valid_pass = [
+                val_fn(xs, ys) for xs, ys in batches(X_val, y_val, 500)
+        ]
+
+        val_err, val_acc = map(sum,zip(*valid_pass))
+        val_batches = len(valid_pass)
+
+        errprint("Epoch {} of {} took {:.3f}s".format(
+            epoch + 1, epochs, time.time() - start_time))
+        errprint("  training loss:\t\t{:.6f}".format(train_err / train_batches))
+        errprint("  validation loss:\t\t{:.6f}".format(val_err / val_batches))
+        errprint("  validation accuracy:\t\t{:.2f} %".format(
+            val_acc / val_batches * 100))
+
+        errprint("Updating intermediate params file")
+        if os.path.exists(tmp_model_file):
+            os.remove(tmp_model_file)
+        np.savez(tmp_model_file, *lasagne.layers.get_all_param_values(network))
+        
+
+    errprint("Finished training. Saving results to file")
+
+    np.savez(model_file, *lasagne.layers.get_all_param_values(network))
+    os.remove(tmp_model_file)
+    return input_var, network
+
+
+def load_predictor():
+    assert os.path.exists(model_file), "Must train model to load predictor"
+        
+    input_var = T.tensor4('inputs')
+    network = build_cnn(input_var)
+    with np.load(model_file) as f:
+        param_values = [f['arr_%d' % i] for i in range(len(f.files))]
+    lasagne.layers.set_all_param_values(network, param_values)
+    
+    prediction = lasagne.layers.get_output(network, deterministic=True)
+    predict_fcn = theano.function([input_var], prediction)
+
+    def predictor(images):
+        """
+Takes an np array of 60x60 normalized greyscale images (shape (1, 60, 60) )
+and returns the top prediction and the confidence of the prediction
+        """
+
+        def get_result(confs):
+            argmax = np.argmax(confs)
+            return (argmax, confs[argmax])
+
+        results = map(get_result, predict_fcn(images))
+        return results
+
+    return predictor
+
+
+if __name__ == "__main__":
+    epochs = 200 if len(sys.argv) < 2 else int(sys.argv[1])
+    train(epochs)
+
+
